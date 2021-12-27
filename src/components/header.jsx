d2787f3e9ec1dbb4696860379440307f4f6c7a47
MIKL <58008533+Mikaelle-S@users.noreply.github.com>
2021-12-27 20:21:05 -0300

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,1 +1,13 @@
-import React from "react";
+import React from 'react';
+
+function Header() {
+    return(
+        <div>
+            <h1>O Processo de Implantação do Novo Serviço Corporativo de <abbr title="Tecnologia da Informação">TI</abbr></h1>
+            <h3><ins>A atualização da terceirização dos serviços.</ins></h3>
+            
+        </div>
+    )
+}
+
+export default Header;
