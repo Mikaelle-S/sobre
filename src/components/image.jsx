109f77f41179c264ea954a80cf65d8fa5f24dee5
Eduarda Cunha <cunhaeduardaf@gmail.com>
2021-12-27 20:27:51 -0300

    add image

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,1 +1,17 @@
-import React from "react";
+import React from 'react';
+import image from '../assets/image.jpg';
+
+function Image() {
+    return(
+        <div>
+        <center>
+            <figure>
+                <img src={image} alt="landscape" width="30%" height="30%"/>
+                <figcaption>Foto aleatória</figcaption>
+            </figure>
+        </center>
+        </div>
+    )
+}
+
+export default Image;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
+import Header from './components/header'
+import Image from './components/image'
 
 ReactDOM.render(
   <div>
-
+    <Header></Header>,
+    <Image></Image>
   </div>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
