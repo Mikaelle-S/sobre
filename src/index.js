4ffdd8c814a23e8eaf3a96dbfaf956a0bb15d3d6
Eduarda Cunha <cunhaeduardaf@gmail.com>
2021-12-27 20:40:32 -0300

    chamando o contents

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Header from './components/header'
 import Image from './components/image'
+import Contents from './components/contents'
 
 ReactDOM.render(
   <div>
-    <Header></Header>,
+    <Header></Header>
     <Image></Image>
+    <Contents></Contents>
   </div>,
   document.getElementById('root')
 );
